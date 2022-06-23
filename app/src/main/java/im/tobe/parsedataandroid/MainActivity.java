6b2b97530ce3aa0381982e9ec902e1ffffda3279
Kenneth Ng <kenneth.ng@lifehikes.com>
2022-06-23 19:51:43 +0800

    Add JsonArrayRequest

--- app/src/main/java/im/tobe/parsedataandroid/MainActivity.java
+++ app/src/main/java/im/tobe/parsedataandroid/MainActivity.java
@@ -9,26 +9,32 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://jsonplaceholder.typicode.com/posts";
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
+        queue = Volley.newRequestQueue(this);
+        getString(queue);
+    }
+
+    private void getString(RequestQueue queue) {
+        String url = "https://jsonplaceholder.typicode.com/comments";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, response -> {
             // display the contents
-            Log.d("Main", "onCreate: "+response.substring(0, 300));
+            Log.d("Main", "onCreate: "+response.toString());
         }, error -> {
-            Log.d("Main", "Failed to get");
+            Log.d("Main", "Failed to get: "+error.getLocalizedMessage());
         });
 
-        queue.add(stringRequest);
+        queue.add(jsonArrayRequest);
     }
 }
